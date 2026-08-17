41947f1f2d96b51ecb7cc6babc9d5a894e4cddee
Dinesh Gupta <61511160+DineshGupta-cloud@users.noreply.github.com>
2026-08-17 14:48:39 +0530

    feat(common-lib): add ErrorResponse DTO

---
+++ common-lib/src/main/java/com/crm/common/response/ErrorResponse.java
@@ -0,0 +1,16 @@
+package com.crm.common.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+
+    private String errorCode;
+    private String message;
+}
