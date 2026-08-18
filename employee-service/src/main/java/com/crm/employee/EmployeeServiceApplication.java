8450c70a80f27e59d4866034dec581c350960f8c
Dinesh Gupta <61511160+DineshGupta-cloud@users.noreply.github.com>
2026-08-18 14:36:36 +0530

    feat(employee-service): add application

---
+++ employee-service/src/main/java/com/crm/employee/EmployeeServiceApplication.java
@@ -0,0 +1,2 @@
+package com.crm.employee; import org.springframework.boot.SpringApplication; import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication public class EmployeeServiceApplication{public static void main(String[] args){SpringApplication.run(EmployeeServiceApplication.class,args);}}
