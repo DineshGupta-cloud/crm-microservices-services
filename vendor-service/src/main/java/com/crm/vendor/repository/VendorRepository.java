0097bd51b498ae020aa918137f59e625c4432e44
Dinesh Gupta <61511160+DineshGupta-cloud@users.noreply.github.com>
2026-08-18 16:20:26 +0530

    feat: add vendor repository

---
+++ vendor-service/src/main/java/com/crm/vendor/repository/VendorRepository.java
@@ -0,0 +1,1 @@
+package com.crm.vendor.repository; import com.crm.vendor.entity.Vendor; import org.springframework.data.jpa.repository.JpaRepository; public interface VendorRepository extends JpaRepository<Vendor,Long>{}
