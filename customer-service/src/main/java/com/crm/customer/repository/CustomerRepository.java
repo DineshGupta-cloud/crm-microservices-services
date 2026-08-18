4878cbf26c8fcea0e3f7282977f1c3a4fe308085
Dinesh Gupta <61511160+DineshGupta-cloud@users.noreply.github.com>
2026-08-18 16:20:18 +0530

    feat: add customer repository

---
+++ customer-service/src/main/java/com/crm/customer/repository/CustomerRepository.java
@@ -0,0 +1,1 @@
+package com.crm.customer.repository; import com.crm.customer.entity.Customer; import org.springframework.data.jpa.repository.JpaRepository; public interface CustomerRepository extends JpaRepository<Customer,Long>{}
