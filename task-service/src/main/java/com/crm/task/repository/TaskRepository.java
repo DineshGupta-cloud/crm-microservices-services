8ee1f49b9243adc5494a97335ad38a03044d85b8
Dinesh Gupta <61511160+DineshGupta-cloud@users.noreply.github.com>
2026-08-18 16:20:40 +0530

    feat: add task repository

---
+++ task-service/src/main/java/com/crm/task/repository/TaskRepository.java
@@ -0,0 +1,1 @@
+package com.crm.task.repository; import com.crm.task.entity.Task; import org.springframework.data.jpa.repository.JpaRepository; public interface TaskRepository extends JpaRepository<Task,Long>{}
