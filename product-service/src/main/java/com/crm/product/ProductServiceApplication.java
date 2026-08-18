085db8f312d0975b9f230f7f81f7dffa121b81dc
Dinesh Gupta <61511160+DineshGupta-cloud@users.noreply.github.com>
2026-08-18 16:18:56 +0530

    feat: add product service application

---
+++ product-service/src/main/java/com/crm/product/ProductServiceApplication.java
@@ -0,0 +1,3 @@
+package com.crm.product;
+import org.springframework.boot.SpringApplication; import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication public class ProductServiceApplication { public static void main(String[] args){SpringApplication.run(ProductServiceApplication.class,args);} }
