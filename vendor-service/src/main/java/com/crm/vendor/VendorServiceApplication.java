f1fc191784ff5051cce9f2d45e6ebe8083992ed9
Dinesh Gupta <61511160+DineshGupta-cloud@users.noreply.github.com>
2026-08-18 16:18:48 +0530

    feat: add vendor service application

---
+++ vendor-service/src/main/java/com/crm/vendor/VendorServiceApplication.java
@@ -0,0 +1,3 @@
+package com.crm.vendor;
+import org.springframework.boot.SpringApplication; import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication public class VendorServiceApplication { public static void main(String[] args){SpringApplication.run(VendorServiceApplication.class,args);} }
